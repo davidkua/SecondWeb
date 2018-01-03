7fa96ee793250dd45a5ffc013b2cce981747dca3
dky <753076382@qq.com>
2018-01-04 01:20:16 +0800

    firstweb测试

---
+++ src/com/keduo/test/Test1.java
@@ -0,0 +1,24 @@
+package com.keduo.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test1
+ */
+public class Test1 extends HttpServlet {
+	 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+	}
+
+}
